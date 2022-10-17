d607e77fdc58336f9bdba8231ee69952947c2daa
beborico1 <beborico16@hotmail.com>
2022-10-17 16:20:23 -0500

    fix loading height

--- components/Product.js
+++ components/Product.js
@@ -5,14 +5,15 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 function Product({front_image}) {
     const [loading,setLoading] = useState(true)
   return (
-    <div className='p-1'>
-        <div className=' relative bg-gray-200'>
-            {loading ? (<div className='text-xl text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>Loading...</div>):(<></>)}
+    <div className='p-1 h-full'>
+        <div className='relative bg-gray-200 h-full'>
             <LazyLoadImage src={front_image}
                 alt="Product"
                 effect="blur"
                 afterLoad={()=>setLoading(false)}
             />
+            {loading ? (<div className='text-xl text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>Loading...</div>):(<></>)}
+            
         </div> 
     </div>
   )
