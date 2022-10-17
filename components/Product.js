d2cf7edcb7a29834236dede1a33a61fe40035b8d
beborico1 <beborico16@hotmail.com>
2022-10-17 16:01:07 -0500

    fix image sizes

--- components/Product.js
+++ components/Product.js
@@ -9,7 +9,6 @@ function Product({front_image}) {
         <div className=' relative bg-gray-200'>
             {loading ? (<div className='text-xl text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>Loading...</div>):(<></>)}
             <LazyLoadImage src={front_image}
-                width={300} height={300}
                 alt="Product"
                 effect="blur"
                 afterLoad={()=>setLoading(false)}
